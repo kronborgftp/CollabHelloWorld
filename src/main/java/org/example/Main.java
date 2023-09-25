046e6b4124cddb0657420c4c6f8c5d95f80d7727
jules0178 <juma0002@stud.kea.dk>
2023-09-25 09:15:51 +0200

    gwejg

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Hello and welcome!");
-        System.out.println(" HELLLOOOOOOOOOOOOOO!! ");
+        System.out.println(" HELLLOOOOOOOOOOOOOOO!! ");
         System.out.println("Goodbyeeee");
 
     }
